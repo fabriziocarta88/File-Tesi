7516f65ea8b0279f34d83146ac1adc48959e1376
fabriziocarta88 <83449373+fabriziocarta88@users.noreply.github.com>
2023-04-26 11:30:43 +0200

    Update main_base_YELP.py

--- SGMP_code-main/main_base_YELP.py
+++ SGMP_code-main/main_base_YELP.py
@@ -317,8 +317,8 @@ def main(data, args):
         if epoch % args.test_per_round == 0:
             valid_loss, yhat_valid, ytrue_valid = test(valid_loader, model, args)
             
-            # Check if the model is YELP_G
-            if args.model == 'YELP_G':
+            # Check if the model is YELP
+            if args.model == 'YELP':
                 loss_function = nn.BCEWithLogitsLoss()
                 valid_loss += loss_function(yhat_valid, ytrue_valid)
 
@@ -352,7 +352,7 @@ def main(data, args):
         if task == 'regression':
             print(f"Final, Train RMSE: {np.sqrt(train_loss):.4f}, Train MAE: {train_score:.4f}, Valid RMSE: {np.sqrt(valid_loss):.4f}, Valid MAE: {valid_score:.4f}, Test RMSE: {np.sqrt(test_loss):.4f}, Test MAE: {test_score:.4f}", file=f)
         elif task == 'classification':
-            if args.model == 'YELP_G':
+            if args.model == 'YELP':
                 print(f"Final, Train loss: {train_loss:.4f}, Train acc: {train_score:.4f}, Valid loss: {valid_loss:.4f}, Valid acc: {valid_score:.4f}, Test loss: {test_loss:.4f}, Test acc: {test_score:.4f}, Binary cross entropy loss: {binary_loss:.4f}", file=f)
             else:
                 print(f"Final, Train loss: {train_loss:.4f}, Train acc: {train_score:.4f}, Valid loss: {valid_loss:.4f}, Valid acc: {valid_score:.4f}, Test loss: {test_loss:.4f}, Test acc: {test_score:.4f}", file=f)
@@ -364,7 +364,7 @@ def main(data, args):
     test_loss, yhat_test, ytrue_test = test(test_loader, best_model, args)
     test_score = measure(ytrue_test, yhat_test)
         
-    if args.model == 'YELP_G':
+    if args.model == 'YELP':
         binary_loss, binary_acc = binary_cross_entropy_loss(yhat_test, ytrue_test)
         with open(result_file, 'a') as f:
             print(f"Best Model, Train loss: {train_loss:.4f}, Train acc: {train_score:.4f}, Valid loss: {valid_loss:.4f}, Valid acc: {valid_score:.4f}, Test loss: {test_loss:.4f}, Test acc: {test_score:.4f}, Binary cross entropy loss: {binary_loss:.4f}", file=f)
