d973f476156ad48f26148bb45025b6c7c56c8d44
fabriziocarta88 <83449373+fabriziocarta88@users.noreply.github.com>
2023-05-02 10:56:35 +0200

    Update SGMP_Y.py

--- SGMP_code-main/models/SGMP_Y.py
+++ SGMP_code-main/models/SGMP_Y.py
@@ -80,18 +80,9 @@ class SGMP(torch.nn.Module):
         thetas = {}
         phis = {}
         i, j, k, p = edge_index_3rd
-        if pos[j][3] == pos[i][3]:
-		i_to_j_dis = float("inf")
-	else:
-		i_to_j_dis = (pos[j] - pos[i]).norm(p=2, dim=1)
-        if pos[j][3] == pos[k][3]:
-		k_to_j_dis = float("inf")
-	else:
-		k_to_j_dis = (pos[k] - pos[j]).norm(p=2, dim=1)
-	if pos[j][3] == pos[p][3]:
-		p_to_j_dis = float("inf")
-	else:
-		p_to_j_dis = (pos[p] - pos[j]).norm(p=2, dim=1)
+        i_to_j_dis = (pos[j] - pos[i]).norm(p=2, dim=1)
+        k_to_j_dis = (pos[k] - pos[j]).norm(p=2, dim=1)
+        p_to_j_dis = (pos[p] - pos[j]).norm(p=2, dim=1)
         
         distances[1] = i_to_j_dis                        
         distances[2] = k_to_j_dis
